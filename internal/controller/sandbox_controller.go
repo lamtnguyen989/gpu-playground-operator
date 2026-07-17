4996cec1bebad3ba83e1b84d958536d84234cb03
Lam Nguyen <lamtnguyen989@gmail.com>
2026-07-17 17:12:38 -0500

    operator-sdk init

---
+++ internal/controller/sandbox_controller.go
@@ -0,0 +1,47 @@
+package controller
+
+import (
+	"context"
+
+	"k8s.io/apimachinery/pkg/runtime"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	logf "sigs.k8s.io/controller-runtime/pkg/log"
+
+	ephemeralv1alpha1 "github.com/lamtnguyen989/gpu-playground-operator/api/v1alpha1"
+)
+
+// SandboxReconciler reconciles a Sandbox object
+type SandboxReconciler struct {
+	client.Client
+	Scheme *runtime.Scheme
+}
+
+// +kubebuilder:rbac:groups=ephemeral.sandbox.gpu-playground.io,resources=sandboxes,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=ephemeral.sandbox.gpu-playground.io,resources=sandboxes/status,verbs=get;update;patch
+// +kubebuilder:rbac:groups=ephemeral.sandbox.gpu-playground.io,resources=sandboxes/finalizers,verbs=update
+
+// Reconcile is part of the main kubernetes reconciliation loop which aims to
+// move the current state of the cluster closer to the desired state.
+// TODO(user): Modify the Reconcile function to compare the state specified by
+// the Sandbox object against the actual cluster state, and then
+// perform operations to make the cluster state reflect the state specified by
+// the user.
+//
+// For more details, check Reconcile and its Result here:
+// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.21.0/pkg/reconcile
+func (r *SandboxReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
+	_ = logf.FromContext(ctx)
+
+	// TODO(user): your logic here
+
+	return ctrl.Result{}, nil
+}
+
+// SetupWithManager sets up the controller with the Manager.
+func (r *SandboxReconciler) SetupWithManager(mgr ctrl.Manager) error {
+	return ctrl.NewControllerManagedBy(mgr).
+		For(&ephemeralv1alpha1.Sandbox{}).
+		Named("sandbox").
+		Complete(r)
+}
